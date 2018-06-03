9ad193fc950c2757e9bd83ea36836bc8f8fc6013
Qi, Ryan (BJG-TIG) <Ryan.Qi@ipgteamignite.com>
2018-06-03 14:56:35 +0800

    添加翻页效果

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,26 @@
 'use strict';
 
 $(document).ready(function () {
-  console.log('doc ready');
+	console.log('doc ready');
+
+	loadFlipBook();
+
+	function loadFlipBook() {
+		var w = $(window).width();
+		var h = $(window).height();
+		$('.js-flip-book').width(w).height(h);
+		$(window).resize(function () {
+			w = $(window).width();
+			h = $(window).height();
+			$('.js-flip-books').width(w).height(h);
+		});
+		if ($(".js-flip-book").length) {
+			$(".js-flip-book").turn({
+				width: w,
+				height: h,
+				autoCenter: true,
+				display: 'single'
+			});
+		}
+	}
 });
--- src/js/_doc-ready.js
+++ src/js/_doc-ready.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
   console.log('doc ready');
+
+  loadFlipBook();
+
+	function loadFlipBook(){
+		let w = $(window).width();
+		let h = $(window).height();
+		$('.js-flip-book').width(w).height(h);
+		$(window).resize(function () {
+			w = $(window).width();
+			h = $(window).height();
+			$('.js-flip-books').width(w).height(h);
+		});
+		if($(".js-flip-book").length){
+			$(".js-flip-book").turn({
+				width: w,
+				height: h,
+				autoCenter: true,
+				display: 'single'
+			});
+		}
+	}
 });
